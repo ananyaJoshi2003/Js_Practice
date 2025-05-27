5d880350db97584c6d29d02faee83e95cb0a5d3e
ananyaJoshi2003 <77530072+ananyaJoshi2003@users.noreply.github.com>
2025-05-27 10:37:03 +0000

    test1

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Hello ji !!");
+// console.log("Hello ji !!");
+
+
+const Id = 1011
+let email = "aj@yash.com"
+var Empname = "annaya Joshi"
+city = "hyderabad"
+
+// Id = 101
+email = "ajoshi@yash.com"
+Empname = "ananya"
+city = "Indore"
+
+console.table([Id, email, Empname, city])
